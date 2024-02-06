ee57ad97762f6274f6f853db88d75d30d0d7f893
Yosenbgb <yosenbgb@gmail.com>
2024-02-06 21:07:33 +0100

    Fix 2

--- app/src/main/java/com/example/progressbarservice/DownloadService.kt
+++ app/src/main/java/com/example/progressbarservice/DownloadService.kt
@@ -1,9 +1,7 @@
 package com.example.progressbarservice
 
 import android.app.Service
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Binder
 import android.os.IBinder
 
@@ -12,16 +10,9 @@ class DownloadService: Service() {
     private val mLocalBinder = LocalBinder()
     private var download = 0
     private var isDownloading = false
-    private lateinit var sharedPreferences: SharedPreferences
 
     inner class LocalBinder: Binder() {
         fun getService(): DownloadService = this@DownloadService
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        sharedPreferences = getSharedPreferences("DownloadProgress", Context.MODE_PRIVATE)
-        download = sharedPreferences.getInt("progress", 0)
     }
 
     override fun onBind(p0: Intent?): IBinder? {
@@ -31,17 +22,12 @@ class DownloadService: Service() {
     fun startDownloading() {
         isDownloading = true
         Thread {
-            while (isDownloading && download <= 100) {
+            while (isDownloading && download < 100) {
                 download++
-                saveProgress(download)
                 Thread.sleep(500)
             }
             isDownloading = false
         }.start()
-    }
-
-    private fun saveProgress(progress: Int) {
-        sharedPreferences.edit().putInt("progress", progress).apply()
     }
 
     fun getProgress(): Int {
